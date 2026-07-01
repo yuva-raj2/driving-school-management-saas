00a6ee00a9d5b55a75ff887d60cb3559c1ff913a
yuva-raj2 <yuvaarun09964@gmail.com>
2026-07-02 00:18:36 +0530

    Updated

---
+++ driving-school-backend/discovery-service/src/main/java/com/dhanalakshmi/discovery/DiscoveryServiceApplication.java
@@ -0,0 +1,10 @@
+package com.dhanalakshmi.discovery;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+}
